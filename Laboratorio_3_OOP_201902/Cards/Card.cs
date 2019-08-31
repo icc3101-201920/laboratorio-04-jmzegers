2fd6c4a889b8c6b73bdd30f729672c43a9022510
Jose Miguel <jmzegers@miuandes.cl>
2019-08-30 21:45:47 -0400

    first commit

---
+++ Laboratorio_3_OOP_201902/Cards/Card.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_3_OOP_201902.Cards
+{
+    public abstract class Card
+    {
+        //Atributos
+        protected string name;
+        protected string EnumType;
+        protected string effect;
+
+        //Constructor
+        public Card()
+        {
+
+        }
+
+        //Propiedades
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return this.EnumType;
+            }
+            set
+            {
+                this.EnumType = value;
+            }
+        }
+        public string Effect
+        {
+            get
+            {
+                return this.effect;
+            }
+            set
+            {
+                this.effect = value;
+            }
+        }
+        
+    }
+}
---
+++ Laboratorio_3_OOP_201902/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Laboratorio_3_OOP_201902
+{
+    public class Enums
+    {
+        public enum EnumType
+        {
+            none,
+            melee,
+            range,
+            longRange,
+            buff,
+            buffMelee,
+            buffRange,
+            buffLongRange,
+            captain,
+            weather
+        }
+    }
+}
